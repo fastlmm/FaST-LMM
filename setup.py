b31cce8d393c4a61188fb1e5805448c2d1712619
Carl Kadie <carlk@msn.com>
2020-01-11 14:11:05 -0800

    Update to lastest pysnptools

--- setup.py
+++ setup.py
@@ -185,7 +185,7 @@ setup(
                        "examples/toydataTrain.phe"
                        ]
                  },
-    install_requires = ['scipy>=0.15.1', 'numpy>=1.11.3', 'pandas>=0.19.0','matplotlib>=1.5.1', 'scikit-learn>=0.19.1', 'pysnptools>=0.4.5', 'dill>=0.2.9', 'mkl>=2019.0'],
+    install_requires = ['scipy>=0.15.1', 'numpy>=1.11.3', 'pandas>=0.19.0','matplotlib>=1.5.1', 'scikit-learn>=0.19.1', 'pysnptools>=0.4.7', 'dill>=0.2.9', 'mkl>=2019.0'],
     cmdclass = cmdclass,
     ext_modules = ext_modules,
   )
