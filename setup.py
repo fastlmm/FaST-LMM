baf61b771d0a85d7772ef8fa34bf1edcbe294da7
Carl Kadie <carlk@msn.com>
2020-04-27 09:28:51 -0700

    travis 30

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ setup(
                  },
     install_requires = ['scipy>=0.15.1', 'numpy>=1.11.3', 'pandas>=0.19.0','matplotlib>=1.5.1',
                        'scikit-learn>=0.19.1', 'pysnptools>=0.4.10', 'dill>=0.2.9', 'mkl>=2019.0',
-                       'statsmodels>=0.10.1', 'psutil>=5.6.3'],
+                       'statsmodels>=0.10.1', 'psutil>=5.6.7'],
     cmdclass = cmdclass,
     ext_modules = ext_modules,
   )
