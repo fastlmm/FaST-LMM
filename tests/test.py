c3362a9200f41f658b8a3c6acdcc9804a25304a3
Carl Kadie <carlk@msn.com>
2024-04-24 16:46:39 -0700

    12

--- tests/test.py
+++ tests/test.py
@@ -177,8 +177,8 @@ if __name__ == "__main__":
             fastlmm.inference.tests.test.getTestSuite(),  # 8
             fastlmm.association.tests.testepistasis.getTestSuite(),  # 9
             fastlmm.association.tests.test_heritability_spatial_correction.getTestSuite(),  # 10
-            # 11 fastlmm.util.test.getTestSuite(),
-            fastlmm.association.tests.test_single_snp_all_plus_select.getTestSuite(),
+            fastlmm.util.test.getTestSuite(),  # 11
+            # 12 fastlmm.association.tests.test_single_snp_all_plus_select.getTestSuite(),
             fastlmm.inference.tests.test.getTestSuite(),
             fastlmm.association.tests.test_single_snp.getTestSuite(),  # special
             # fastlmm.association.tests.test_single_snp_linreg.getTestSuite(),
