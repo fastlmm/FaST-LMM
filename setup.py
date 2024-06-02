c1c0ee2f385ad5f978d5961a192d357af2dc18e0
Carl Kadie <carlk@msn.com>
2024-06-02 11:13:57 -0700

    Try beta 3 of pysnptools

--- setup.py
+++ setup.py
@@ -94,11 +94,11 @@ setup(
         "matplotlib>=1.5.1",
         "scikit-learn>=0.19.1",
         "bed-reader>=1.0.4",
-        "pysnptools>=0.5.12b",  # cmk
+        "pysnptools>=0.5.12b3",  # cmk
         "cloudpickle>=2.2.0",
         "statsmodels>=0.10.1",
         "psutil>=5.6.7",
         "fastlmmclib>=0.0.3",
     ],
-    extras_require={"bgen": ["pysnptools[bgen]>=0.5.12b"]},
+    extras_require={"bgen": ["pysnptools[bgen]>=0.5.12b3"]},
 )
